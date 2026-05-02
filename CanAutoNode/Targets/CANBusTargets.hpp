e304af1af416bdcf049aa716e80f27ef477e12b3
shivamdesai04 <shivam.desai@ucalgary.ca>
2026-05-02 14:04:33 -0600

    Added Aegis CAN Targets

---
+++ CanAutoNode/Targets/CANBusTargets.hpp
@@ -0,0 +1,66 @@
+/**
+ ********************************************************************************
+ * @file    CANBusTargets.hpp
+ * @author  Shivam Desai
+ * @date    May 2, 2026
+ * @brief
+ ********************************************************************************
+ */
+
+#ifndef CANBUSTARGETS_HPP_
+#define CANBUSTARGETS_HPP_
+
+/************************************
+ * INCLUDES
+ ************************************/
+
+/************************************
+ * MACROS AND DEFINES
+ ************************************/
+/**
+ * ROCKET TARGETS
+ */
+
+// Mother Target
+#define CAN_ROCKET_TARGET_FCB "FCB"
+
+// Daughter Targets
+#define CAN_ROCKET_TARGET_BMB "BMB"
+#define CAN_ROCKET_TARGET_DAQ "DAQ"
+#define CAN_ROCKET_TARGET_RPB "RPB"
+
+/**
+ * GSE TARGETS
+ */
+
+// Mother Target
+#define CAN_GSE_TARGET_FSB "FSB"
+
+// Daughter Targets
+#define CAN_GSE_TARGET_TC1 "TC1"
+#define CAN_GSE_TARGET_TC2 "TC2"
+#define CAN_GSE_TARGET_TC3 "TC3"
+
+// Daughter Targets
+#define CAN_GSE_TARGET_PT1 "PT1"
+#define CAN_GSE_TARGET_PT2 "PT2"
+#define CAN_GSE_TARGET_PT3 "PT3"
+
+// Daughter Targets
+#define CAN_GSE_TARGET_LC1 "LC1"
+#define CAN_GSE_TARGET_LC2 "LC2"
+#define CAN_GSE_TARGET_LC3 "LC3"
+
+/************************************
+ * TYPEDEFS
+ ************************************/
+
+/************************************
+ * CLASS DEFINITIONS
+ ************************************/
+
+/************************************
+ * FUNCTION DECLARATIONS
+ ************************************/
+
+#endif /* CANBUSTARGETS_HPP_ */
