4dc6c85ee415996f009413c369aaf21a48a70004
shivamdesai04 <shivam.desai@ucalgary.ca>
2026-05-02 14:26:13 -0600

    added imu types

--- CanAutoNode/LogTypes/LogStructs.hpp
+++ CanAutoNode/LogTypes/LogStructs.hpp
@@ -8,6 +8,48 @@
 #ifndef LOGSTRUCTS_HPP_
 #define LOGSTRUCTS_HPP_
 
+// Possible IMU structs
+struct CanImuData9Axis {
+	uint32_t accel_x;
+	uint32_t accel_y;
+	uint32_t accel_z;
+	uint32_t gyro_x;
+	uint32_t gyro_y;
+	uint32_t gyro_z;
+	uint32_t mag_x;
+	uint32_t mag_y;
+	uint32_t mag_z;
+};
+
+struct CanImuData6Axis {
+	uint32_t accel_x;
+	uint32_t accel_y;
+	uint32_t accel_z;
+	uint32_t gyro_x;
+	uint32_t gyro_y;
+	uint32_t gyro_z;
+};
+
+struct CanAccelData {
+	uint32_t accel_x;
+	uint32_t accel_y;
+	uint32_t accel_z;
+};
+
+struct CanGyroData {
+	uint32_t gyro_x;
+	uint32_t gyro_y;
+	uint32_t gyro_z;
+};
+
+struct CanMagData {
+	uint32_t mag_x;
+	uint32_t mag_y;
+	uint32_t mag_z;
+};
+
+
+// Template Examples - Will be removed later
 struct ExampleDataA {
 	uint32_t a;
 	uint8_t b;
