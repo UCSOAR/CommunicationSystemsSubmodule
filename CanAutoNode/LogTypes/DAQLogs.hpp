2947756b1ae6c776d351241ee9097db04f23f5a5
Adam <145816785+b-atteryacid@users.noreply.github.com>
2026-05-03 15:33:41 -0600

    add _COUNT field to CAN Logs (#8)
    Added _COUNT

--- CanAutoNode/LogTypes/DAQLogs.hpp
+++ CanAutoNode/LogTypes/DAQLogs.hpp
@@ -27,7 +27,7 @@ const CanAutoNodeDaughter::LogInit DAQ_Init[] = {DAQ_LOGS_X};
 #undef X
 
 #define X(x) _ ## x ## _LOGINDEX,
-enum DAQ_LogIndexes {DAQ_LOGS_X};
+enum DAQ_LogIndexes {DAQ_LOGS_X _COUNT};
 #undef X
 
 #endif /* DAQLOGS_HPP_ */
