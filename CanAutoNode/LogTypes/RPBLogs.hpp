a6285cf697b965460767ad62abf10b30e4416189
Adam <145816785+b-atteryacid@users.noreply.github.com>
2026-05-05 20:55:03 -0600

    Add DAQ->RPB airbrakes level command (#9)
    Add CAN command for daq to rpb telling it to set the airbrakes level
    (which is different from the fcb telling it to enable/disable)
    
    ---------
    
    Co-authored-by: shivamdesai04 <shivam.desai@ucalgary.ca>

--- CanAutoNode/LogTypes/RPBLogs.hpp
+++ CanAutoNode/LogTypes/RPBLogs.hpp
@@ -25,11 +25,16 @@ struct RPB_AIR_BRAKES_COMMAND {
 	bool openAirBrakes;
 };
 
+struct RPB_FROM_DAQ_AIR_BRAKES_LEVEL {
+	uint8_t level;
+};
+
 // Add CAN logs here in the order you want them
 #define RPB_CAN_LOGS \
 	X(RPB_CAMERA_POWER_COMMAND) \
 	X(RPB_CAMERA_RECORDING_COMMAND) \
-	X(RPB_AIR_BRAKES_COMMAND)
+	X(RPB_AIR_BRAKES_COMMAND) \
+	X(RPB_FROM_DAQ_AIR_BRAKES_LEVEL)
 
 #define X(x) sizeof(x),
 const CanAutoNodeDaughter::LogInit RPB_Init[] = {RPB_CAN_LOGS};
