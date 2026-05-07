7ab1648e05cc530d590e845b1bd02a4870b0c2f1
Spiro Douvis <spirodouvis@gmail.com>
2026-05-06 18:19:30 -0600

    Changed path for canbustargets.hpp

--- CanAutoNode/CanAutoNodeMotherboard.hpp
+++ CanAutoNode/CanAutoNodeMotherboard.hpp
@@ -2,7 +2,7 @@
 #define CANAUTONODEMOTHERBOARD_HPP_
 #include "FDCan.h"
 #include "CanAutoNode.hpp"
-#include "Targets/CANBusTargets.hpp"
+#include "CANBusTargets.hpp"
 
 class CanAutoNodeMotherboard : public CanAutoNode {
 
