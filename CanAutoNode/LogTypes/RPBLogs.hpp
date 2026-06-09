9adbd51deffc720c021e3eb95b48841ff095df83
Local user <goadam80@gmail.com>
2026-06-08 20:31:25 -0600

    added camera button command

--- CanAutoNode/LogTypes/RPBLogs.hpp
+++ CanAutoNode/LogTypes/RPBLogs.hpp
@@ -37,6 +37,14 @@ struct RPB_FROM_DAQ_AIR_BRAKES_LEVEL {
 	uint8_t level;
 };
 
+struct RPB_CAMERA_SIMULATE_BUTTON_COMMAND {
+	uint8_t cam;
+	// 0x00 wifi btn
+	// 0x01 power btn
+	// 0x02 mode switch
+	uint8_t button;
+};
+
 #ifdef PACK_CAN_STRUCTS
 #pragma pack(pop)
 #endif
@@ -46,7 +54,8 @@ struct RPB_FROM_DAQ_AIR_BRAKES_LEVEL {
 	X(RPB_CAMERA_RECORDING_COMMAND) \
 	X(RPB_CAMERA_SWITCH_COMMAND) \
 	X(RPB_AIR_BRAKES_COMMAND) \
-	X(RPB_FROM_DAQ_AIR_BRAKES_LEVEL)
+	X(RPB_FROM_DAQ_AIR_BRAKES_LEVEL) \
+	X(RPB_CAMERA_SIMULATE_BUTTON_COMMAND)
 
 #define X(x) sizeof(x),
 const CanAutoNodeDaughter::LogInit RPB_Init[] = {RPB_CAN_LOGS};
